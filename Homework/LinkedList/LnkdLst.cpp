eab36a5f261ad90dfae00e5d43e808f9a3b2c767
eg2072664 <ifrit39@hotmail.com>
2014-09-30 22:56:13 -0700

    Additions to Linked List
    Incomplete - started the copy constructor

--- Homework/LinkedList/LnkdLst.cpp
+++ Homework/LinkedList/LnkdLst.cpp
@@ -12,17 +12,39 @@ LnkdLst::LnkdLst(int firstData){
     head = new Node;
     head->data = firstData;
     head->next = 0;
+    head = tail;
+}
+
+//TODO fix this function
+LnkdLst::LnkdLst(const LnkdLst &copy){
+    Node copyWorker;
+    if(copy.head != 0){
+        head = new Node;
+        head->data = copy.head->data;
+        copyWorker = copy.head->next;
+        worker = head;
+        while(copy.worker->next != 0){
+            worker->next = new Node;
+            worker->data = copy.worker->data;
+
+            temp = copy.worker->next;
+        }
+    else head = 0;
+    }
 }
 
 void LnkdLst::append(int data){
-    worker = head;
-    while(worker->next != 0){
-        worker = worker->next;
-    }
-    worker->next = new Node;
-    worker = worker->next;
-    worker->data = data;
-    worker->next = 0;
+    tail->next = new Node;
+    tail = tail->next;
+    tail->data = data;
+    tail->next = 0;
+}
+
+void LnkdLst::prepend(int data){
+    Node newNode = new Node;
+    newNode->next = head;
+    newNode->data = data;
+    head = newNode;
 }
 
 string LnkdLst::toString(){
@@ -43,6 +65,6 @@ LnkdLst::~LnkdLst(){
     while(worker->next != 0){
         head = worker;
         worker = worker->next;
-        delete []head;
+        delete head;
     }
 }
--- Homework/LinkedList/LnkdLst.h
+++ Homework/LinkedList/LnkdLst.h
@@ -13,9 +13,22 @@ using namespace std;
 
 class LnkdLst {
 public:
+    //Constructors
     LnkdLst(int);
-    void append(int);
+    LnkdLst(const LnkdLst &);
+    LnkdLst &operator=(const LnkdLst &);
+
+    //Getters
+    int first();
+    int last();
     string toString();
+
+    //add Node functions
+    void append(int);
+    void prepend(int);
+    void insertAfter(int, int);
+    void insertBefore(int, int);
+
     virtual ~LnkdLst();
 private:
     struct Node{
@@ -24,6 +37,7 @@ private:
     };
     Node *head;
     Node *worker;
+    Node *tail;
 };
 
 #endif	/* LNKDLST_H */
